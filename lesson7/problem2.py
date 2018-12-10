c1149d551873f0aebe6e978c9144e447d43f6a89
Jchandler01 <43756533+Jchandler01@users.noreply.github.com>
2018-12-10 12:36:39 -0500

    Add files via upload

---
+++ lesson7/problem2.py
@@ -0,0 +1,17 @@
+print('*' * 65)
+print('Hi I am Multiplyer bot!')
+
+print()
+
+x = input("pick a number ")
+X = int(x)
+y = input('pick another number ')
+y = int(y)
+
+product = x * y
+
+print('The answer is ' + str(product))
+
+print('*' * 65)
+
+print()
---
+++ lesson7/problem3.py
@@ -0,0 +1,20 @@
+print('-' * 65)
+
+print('100th Birthday program ')
+print()
+
+print('Description: This program asks for your current age and tells you the year that you will turn 100.')
+
+x = input('What is your age today? ')
+x = int(x)
+
+product = 2018 - x + 100
+
+print('You will turn 100 in the year ' + str(product))
+
+print('-' * 65)
+print()
+
+
+
+
